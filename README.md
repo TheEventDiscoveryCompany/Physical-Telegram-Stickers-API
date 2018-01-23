72704a67fe4f24291fcf0510a017805415b64f59
Andrew Jarvis <ajarvis7771@gmail.com>
2018-01-23 15:51:02 -0500

    initial codebase

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Physical-Telegram-Stickers-API
 A🅱️I for PTS yo
+
+## Updating submodules
+To install/update submodules:
+```bash
+git submodule update --init --recursive --remote
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+//Mongoose stuff
+mongoose.Promise = global.Promise;
+var Chat = require('./models/mongo/Chat'),
+    StickerGroup = require('./models/mongo/StickerGroup'),
+    Sticker = require('./models/mongo/Sticker');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // for parsing application/x-www-form-urlencoded
+
+var port = process.env.PORT || 3000;
+
+if (process.env.MONGODB_URI === undefined) {
+    // Server with no authentication
+    var mongodbUri = "mongodb://";
+    mongodbUri += process.env.MONGODB_HOST + ":" +
+    process.env.MONGODB_PORT + "/" +
+    process.env.MONGODB_DATABASE;
+}
+else {
+    // URI set on production/staging
+    var mongodbUri = process.env.MONGODB_URI;
+}
+
+app.get('/', function(req, res) {
+    res.send("Welcome friend ( ͡° ͜ʖ ͡°)");
+});
+
+
+app.listen(port, function() {
+    console.log('🅱️erver listening on port 3000');
+});
