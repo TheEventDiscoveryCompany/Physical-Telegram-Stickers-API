9ac6cbf880dfb1102a9e6491b9def706c39f2c7e
Andrew Jarvis <ajarvis7771@gmail.com>
2018-02-06 18:01:50 -0500

    Added create sticker group endpoint

--- controllers/StickerGroupController.js
+++ controllers/StickerGroupController.js
@@ -4,16 +4,49 @@ var Helpers = require('../helpers/Helpers');
 var NotFoundError = require('../helpers/NotFoundError');
 var StickerGroup = require('../models/StickerGroup');
 
-router.get('/:urlSlug', (req, res, next) => {
-    const stickerGroup = StickerGroup
-        .query(req.knex)
-        .where('urlSlug', req.params.urlSlug)
-        .first();
+
+// Create sticker group
+router.post('/', (req, res, next) => {
+    var errors = {};
+
+    // Verify parameters
+    if (req.body.chatId == null) {
+        errors.chatId = "Please provide a chat ID";
+    }
+    else if (isNaN(req.body.chatId)) {
+        errors.chatId = "Please provide a numeric chat ID";
+    }
+
+    if (!Helpers.isObjectEmpty(errors)) {
+        const responseJson = Helpers.getResponseJson(errors, "Invalid request data");
+        res.status(400).json(responseJson);
+    }
+    else {
+        // Create new sticker group
+        const newStickerGroup = StickerGroup.query(req.knex).insert({ chatId: parseInt(req.body.chatId, 10) });
+
+        newStickerGroup.then(stickerGroup => {
+            console.log(stickerGroup);
+            const responseJson = Helpers.getResponseJson(stickerGroup);
+            res.status(200).json(responseJson);
+        })
+        .catch(err => {
+            console.log(err);
+            const responseJson = Helpers.getResponseJson({}, "Could not create sticker group");
+            res.status(500).json(responseJson);
+        });
+    }
+});
+
+
+// Read sticker group
+router.get('/:id', (req, res, next) => {
+    const stickerGroup = StickerGroup.query(req.knex).where('id', req.params.id).first();
 
     stickerGroup.then(stickerGroup => {
         console.log("Got sticker group: ", stickerGroup);
         if (stickerGroup == null) {
-            return Promise.reject(new NotFoundError("Could not find a sticker group with that url slug"));
+            return Promise.reject(new NotFoundError("Could not find a sticker group with that id"));
         }
 
         var responseJson = Helpers.getResponseJson(stickerGroup);
