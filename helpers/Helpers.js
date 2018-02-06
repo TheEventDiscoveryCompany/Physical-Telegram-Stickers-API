0c393d07178d6ccad38662c899436b114bddab34
Andrew Jarvis <ajarvis7771@gmail.com>
2018-02-06 18:02:50 -0500

    helper fn

--- helpers/Helpers.js
+++ helpers/Helpers.js
@@ -25,6 +25,10 @@ class Helpers {
                 .catch(next);
         };
     }
+
+    static isObjectEmpty(obj) {
+        return (Object.keys(obj).length === 0 && obj.constructor === Object);
+    }
 }
 
 module.exports = Helpers;
